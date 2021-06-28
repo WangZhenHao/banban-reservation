b9701d6c46dfbc7f5480c3d7713338a8672d4eac
udream-Wangzh <wangzh@udream.cn>
2021-06-28 14:44:38 +0800

    [feat] 添加预约模拟机弹出

--- src/views/home/analogueMachine/components/appointment.vue
+++ src/views/home/analogueMachine/components/appointment.vue
@@ -1,39 +1,71 @@
 <template>
     <el-dialog
-        title="预约"
         :visible.sync="dialogVisible"
-        width="640px">
+        title="预约"
+        width="640px"
+    >
         <div>
-            sdfsdfsdf
+            <div>
+                <el-button
+                    size="mini"
+                    :key="item.SimId"
+                    @click="selectHandle(item)"
+                    class="list-item"
+                    v-for="(item) in list"
+                    :disabled="item.isOrder === 0"
+                >{{ item.name }}</el-button>
+            </div>
         </div>
-        <span slot="footer" class="dialog-footer">
+        <span
+            class="dialog-footer"
+            slot="footer"
+        >
             <el-button @click="dialogVisible = false">取 消</el-button>
-            <el-button type="primary" @click="confirm">确 定</el-button>
+            <el-button
+                @click="confirm"
+                type="primary"
+            >确 定</el-button>
         </span>
     </el-dialog>
 </template>
 <script>
-import { getSimulatorNumber } from '@api/analogue'
+import { getSimulatorNumber } from '@api/analogue';
 export default {
     data() {
         return {
             dialogVisible: false,
-            list: []
-        }
+            list: [],
+            selectItem: {},
+        };
     },
     methods: {
         init(info) {
             this.dialogVisible = true;
             this.info = info;
-            getSimulatorNumber(info).then(res => {
-
-            }).catch(() => {
-
-            })
+            console.log(this.$parent);
+            getSimulatorNumber(info)
+                .then((res) => {
+                    this.list = res.result;
+                })
+                .catch(() => {});
         },
-        confirm() {
-
-        }
-    }
-}
+        selectHandle(item) {
+            this.selectItem = item;
+        },
+        confirm() {},
+    },
+};
 </script>
+<style lang="scss" scoped>
+// .list-item {
+//     padding: 6px 10px;
+//     background: #409eff;
+//     border-radius: 100px;
+//     margin-left: 20px;
+//     cursor: pointer;
+//     user-select: none;
+// }
+// .list-item__active {
+//     background: ;
+// }
+</style>
