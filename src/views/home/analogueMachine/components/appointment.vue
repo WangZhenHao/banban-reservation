ad6cbba8d2772ab1036cbd629e833c461de10f32
udream-Wangzh <wangzh@udream.cn>
2021-06-28 11:04:21 +0800

    【feat】添加预约组件

---
+++ src/views/home/analogueMachine/components/appointment.vue
@@ -0,0 +1,39 @@
+<template>
+    <el-dialog
+        title="预约"
+        :visible.sync="dialogVisible"
+        width="640px">
+        <div>
+            sdfsdfsdf
+        </div>
+        <span slot="footer" class="dialog-footer">
+            <el-button @click="dialogVisible = false">取 消</el-button>
+            <el-button type="primary" @click="confirm">确 定</el-button>
+        </span>
+    </el-dialog>
+</template>
+<script>
+import { getSimulatorNumber } from '@api/analogue'
+export default {
+    data() {
+        return {
+            dialogVisible: false,
+            list: []
+        }
+    },
+    methods: {
+        init(info) {
+            this.dialogVisible = true;
+            this.info = info;
+            getSimulatorNumber(info).then(res => {
+
+            }).catch(() => {
+
+            })
+        },
+        confirm() {
+
+        }
+    }
+}
+</script>
