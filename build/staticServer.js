e63e68c911bbb1e7952eae7ef660c2b3974f94d9
udream-Wangzh <wangzh@udream.cn>
2022-03-28 14:10:40 +0800

    【fix】修改端口

--- build/staticServer.js
+++ build/staticServer.js
@@ -5,5 +5,5 @@ const app = new Koa()
 
 
 app.use(static(path.join(__dirname, '../dist')));
-app.listen(3002, '0.0.0.0');
-console.log('static server is staring at port 3002');
+app.listen(8001, '0.0.0.0');
+console.log('static server is staring at port 8001');
